25e2a46ab678c735e9f3ef2e21c950ece8a5d6e5
Ruben <a.r.c.broere@students.uu.nl>
2023-09-13 16:52:23 +0200

    feat: moved extensions to folder

--- Extensions/ByteArrayExtentions.cs
+++ Extensions/ByteArrayExtentions.cs
@@ -2,11 +2,21 @@
 
 public static class ByteArrayExtentions
 {
+    /// <summary>
+    /// Apply a filter funtion to the entire array
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="func"></param>
     public static void Apply(this byte[,] input, Func<byte[,], byte[,]> func)
     {
         func(input);
     }
 
+    /// <summary>
+    /// Apply byte to byte function on the entire array
+    /// </summary>
+    /// <param name="array"></param>
+    /// <param name="func"></param>
     public static void PointOperation(this byte[,] array, Func<byte, byte> func)
     {
         var width = array.GetLength(0);
