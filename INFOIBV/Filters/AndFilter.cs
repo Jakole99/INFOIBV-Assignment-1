16743e50c0552d6cd46a5ec52ce5c4d505dcfc81
Jakole99 <jandre99@live.nl>
2023-10-05 15:00:58 +0200

    AND and OR

---
+++ INFOIBV/Filters/AndFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFOIBV.Framework;
+
+namespace INFOIBV.Filters;
+
+public class AndFilter : Filter
+{
+    public override string Name => "andImage";
+
+    private readonly byte[,] _input2;
+
+    public AndFilter(byte[,] input)
+    {
+        _input2 = input;
+    }
+    protected override byte ConvertPixel(int u, int v, byte[,] input)
+    {
+        //With a Threshold "check", so if an user doesn't give a binary image then we will just make it binary.
+        if (input[u, v] < Byte.MaxValue / 2 && _input2[u, v] > Byte.MaxValue / 2)
+            return Byte.MinValue;
+        return Byte.MaxValue;
+    }
+}
+
+
+public partial class PipelineExtensions
+{
+    public static PipeLine AddAndFilter(this PipeLine pipeLine, byte[,] input)
+    {
+        return pipeLine.AddFilter(new AndFilter(input));
+    }
+}
---
+++ INFOIBV/Filters/OrFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFOIBV.Framework;
+
+namespace INFOIBV.Filters;
+
+public class OrFilter : Filter
+{
+    public override string Name => "orImage";
+
+    private readonly byte[,] _input2;
+
+    public OrFilter(byte[,] input)
+    {
+        _input2 = input;
+    }
+    protected override byte ConvertPixel(int u, int v, byte[,] input)
+    {
+        //With a Threshold "check", so if an user doesn't give a binary image then we will just make it binary.
+        if (input[u, v] < Byte.MaxValue / 2 || _input2[u, v] > Byte.MaxValue / 2)
+            return Byte.MinValue;
+        return Byte.MaxValue;
+    }
+}
+
+
+public partial class PipelineExtensions
+{
+    public static PipeLine AddOrFilter(this PipeLine pipeLine, byte[,] input)
+    {
+        return pipeLine.AddFilter(new OrFilter(input));
+    }
+}
